97bc0cf52af4779877f5502f763a9efb245990c5
Miguel <miguel.bugalho@gmail.com>
2019-10-21 10:45:05 +0100

    Added a method to open modal windows
    The shop list window is now opened as a modal window
    We can also do "showAndWait" to retrieve information if needed

--- src/pt/iade/shoplist/WindowManager.java
+++ src/pt/iade/shoplist/WindowManager.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import pt.iade.shoplist.controllers.MainController;
 import pt.iade.shoplist.controllers.ShopListController;
@@ -23,13 +24,27 @@ public class WindowManager {
 	public static void openMainWindow() {
 		openWindow("views/mainView.fxml",
 				primaryStage,new MainController());
+		primaryStage.show();
 	}
 
 	public static void openShopListWindow(ShopList shopList) {
-		openWindow("views/shopListView.fxml",new Stage(),
-				new ShopListController(shopList));
+		Stage stage = new Stage();
+		openModalWindow("views/shopListView.fxml",stage,
+				primaryStage,new ShopListController(shopList));
+		 
+		System.out.println("INICIO");
+		stage.showAndWait();
+		System.out.println("FIM");
 	}
 
+	
+	public static void openModalWindow(String viewPath, Stage window,
+			Stage parentWindow, Object controller) {
+		window.initOwner(parentWindow);
+		window.initModality(Modality.APPLICATION_MODAL); 
+		openWindow(viewPath, window,controller);
+		
+	}
 	
 
 	public static void openWindow(String viewPath, Stage window, 
@@ -42,7 +57,6 @@ public class WindowManager {
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(Main.class.getResource("application.css").toExternalForm());
 			window.setScene(scene);
-			window.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
